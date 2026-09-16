1e29c1f987a09f717e1d55cbb95c140e7d03321a
Ludovic-Lesur <ludovic.lesur46@gmail.com>
2026-09-16 08:22:25 +0200

    Fix multiple inclusion in error driver.

--- inc/error.h
+++ inc/error.h
@@ -60,7 +60,6 @@ ERROR_code_t ERROR_stack_read(void);
  *******************************************************************/
 uint8_t ERROR_stack_is_empty(void);
 
-#ifdef EMBEDDED_UTILS_ERROR_STACK_SIGFOX
 /*!******************************************************************
  * \fn void ERROR_import_sigfox_stack(void)
  * \brief Import the Sigfox EP library error stack in the MCU stack.
@@ -69,7 +68,6 @@ uint8_t ERROR_stack_is_empty(void);
  * \retval      none
  *******************************************************************/
 void ERROR_import_sigfox_stack(void);
-#endif
 
 /*******************************************************************/
 #define ERROR_check_exit(ll_status, ll_success, ll_base)  { if (ll_status != ll_success) { status = (((ll_base) << 0) + ll_status); goto errors; } }
